9e2ba3822735d695310d1b762aa05b1016cf06d6
Muhammadaziz <muhammadazizravshanbekov107@gmail.com>
2021-08-15 12:45:05 +0500

    Add

--- js/modal.js
+++ js/modal.js
@@ -8,3 +8,29 @@ elModalBtn.addEventListener("click", function () {
 elModalClosebtn.addEventListener("click", function () {
   elModal.classList.remove("modal--active");
 });
+
+var elForm = document.querySelector(".form");
+var elInput = elForm.querySelector(".form-input");
+var elResult1 = document.querySelector(".result1");
+var elResult2 = document.querySelector(".result2");
+var elResult3 = document.querySelector(".result3");
+var elResult4 = document.querySelector(".result4");
+var menSpeed = 3.6;
+var bikeSpeed = 20.1;
+var carSpeed = 70;
+var planeSpeed = 800;
+
+function checkTime(distance, speed) {
+  return distance / speed;
+}
+
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  var numberResult = Number(elInput.value.trim());
+
+  elResult1.textContent = checkTime(numberResult, menSpeed);
+  elResult2.textContent = checkTime(numberResult, bikeSpeed);
+  elResult3.textContent = checkTime(numberResult, carSpeed);
+  elResult4.textContent = checkTime(numberResult, planeSpeed);
+});
